8fa341d2589de25bbb0289483d15215f51538451
Lino Tadros <lino@eventsxd.com>
2021-07-19 13:15:28 -0400

    My First Checkin

---
+++ Cotiviti/Script/Unit1.js
@@ -0,0 +1,21 @@
+﻿function foo()
+{
+  //Log.Message(Sys.Process("Orders").WinFormsObject("MainForm").WinFormsObject("StatusBar").wText(0));
+  //Log.Message(Aliases.Orders.MainForm.StatusBar.wText(0));
+  // Log.Message(Aliases.StatusBar.wText(0));
+  Log.Message(NameMapping.Sys.Cotiviti.Form1.groupBox1.MyNewTextbox.wText);
+}
+
+function Test1()
+{
+  Log.Message(Aliases.Cotiviti.MyNewTextbox.wText);
+}
+
+function Test2()
+{
+  let form1 = Aliases.Cotiviti.Form1;
+  let textBox = form1.textBox1;
+  textBox.Click(31, 5);
+  textBox.SetText("Alain");
+  form1.button1.ClickButton();
+}
